a24ac5fc582250c456f24615675d179a45e42bb7
Moch Ridwan Taufik Alamsyah <ridwan@csacomputer.com>
2022-04-12 10:27:53 +0700

    feat: add update system listener

--- src/main/system/index.js
+++ src/main/system/index.js
@@ -7,8 +7,42 @@ const rootPath = app.isPackaged === false ? app.getAppPath() : path.dirname(app.
 const config = ini.parse(fs.readFileSync(path.resolve(rootPath + '/wacsa.ini'), 'utf-8'));
 const versionTag = app.getVersion();
 
+const updateListener = (autoUpdater, ipcMain, win, errorLogger) => {
+  autoUpdater.autoDownload = false;
+
+  autoUpdater.on('error', async (error) => {
+    win.webContents.send('update_error', error);
+    await errorLogger('electron #autoUpdater' + error, win);
+  });
+
+  autoUpdater.on('update-available', (info) => {
+    win.webContents.send('update_available', info);
+  });
+
+  autoUpdater.on('update-not-available', (info) => {
+    win.webContents.send('update_not_available', info);
+  });
+
+  autoUpdater.on('update-downloaded', () => {
+    win.webContents.send('update_downloaded');
+  });
+
+  ipcMain.on('check-for-update', () => {
+    autoUpdater.checkForUpdates().catch(() => { });
+  });
+
+  ipcMain.on('download-update', () => {
+    autoUpdater.downloadUpdate();
+  });
+
+  ipcMain.on('restart-for-update', () => {
+    autoUpdater.quitAndInstall();
+  });
+}
+
 module.exports = {
   rootPath,
   config,
   versionTag,
+  updateListener
 };
